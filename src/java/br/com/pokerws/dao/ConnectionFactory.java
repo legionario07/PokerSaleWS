db92c8c367c69c5a3ea87fe44c20b10c4c39255d
Paulo Dias <paulinho.legionario@hotmail.com>
2018-11-06 16:35:22 -0200

    Update ConnectionFactory.java

--- src/java/br/com/pokerws/dao/ConnectionFactory.java
+++ src/java/br/com/pokerws/dao/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
 
 	private static String url = "jdbc:mysql://31.220.104.175:3306/u517145271_poker??autoReconnect=true";
 	private static String usuario = "u517145271_root";
-	private static String senha = "poker07";
+	private static String senha = "*****";
         
         
         
